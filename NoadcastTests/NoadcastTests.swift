c70f5751b3ff62fd2882244780433c4efb6b055a
Isaac Khor <s.github@isaackhor.com>
2026-05-15 12:39:10 -0400

    Initial Commit

---
+++ NoadcastTests/NoadcastTests.swift
@@ -0,0 +1,19 @@
+//
+//  NoadcastTests.swift
+//  NoadcastTests
+//
+//  Created by Isaac Khor on 2026.05.15.
+//
+
+import Testing
+@testable import Noadcast
+
+struct NoadcastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
